7cab4ed288e320e5b67555a96afad2605ab53008
Shubham Shankar <rathsam4@gmail.com>
2022-11-23 21:15:09 -0600

    Inheritence

---
+++ basic_contract/InheritSimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+// Import other file
+import "./SimpleStorage.sol";
+
+// is keyword is used for inheritance.
+contract InheritStorageFactory is SimpleStorage{
+    // creating an simple storage array
+    SimpleStorage[] public simpleStorageArray;
+
+
+    // function to deploy simplestorage contract
+    function createSimpleStorageContract() public {
+        // obj-type var = new contract();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    // callling function from SimpleStorage
+    function StorageFactoryStore(uint256 _simpleStorageIndex, uint256 _favouriteNumber) public {
+        // address of contract we intract with
+        // ABI = Application Binary Interface
+        // return contract
+        SimpleStorage simpleStrorageContract = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        simpleStrorageContract.store(_favouriteNumber);
+    }
+
+    function StorageFactoryRetrive(uint256 _simpleStorageIndex) public view returns (uint256){
+        SimpleStorage simpleStrorageContract = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
+        return simpleStrorageContract.retrive();
+    }
+
+}
